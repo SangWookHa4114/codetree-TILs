71f3d6a25ffd13559538877dbc927ba251ebed00
SangWookHa4114 <128560385+SangWookHa4114@users.noreply.github.com>
2024-08-27 16:02:45 +0900

    Title: 힙 정렬 구현, Time: 10ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240827/힙 정렬 구현/implement-heap-sort.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+void heapify(int now,vector<int>&arr,int n);
+void heap_sort(vector<int>&arr,int n);
+int main(){
+    int n;cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++)cin>>arr[i];
+    heap_sort(arr,n);
+    for(auto i:arr)cout<<i<<" ";
+}
+void heapify(int now,vector<int>&arr,int n){
+    int left=now*2+1,right=now*2+2,largest=now;
+    if(left<n && arr[left]>arr[now])largest=left;
+    if(right<n && arr[right]>arr[now] && arr[right]>arr[left])largest=right;
+    if(largest!=now){
+        swap(arr[now],arr[largest]);
+        heapify(largest,arr,n);
+    }
+}
+void heap_sort(vector<int>&arr,int n){
+    for(int i=n/2-1;i>=0;i--)heapify(i,arr,n);
+    for(int i=n-1;i>0;i--){
+        swap(arr[0],arr[i]);
+        heapify(0,arr,i-1);
+    }
+}
